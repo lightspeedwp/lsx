6972d62222646a96c5f78502ee153557b4d7bd79
richlloydmiles <Lightspeed@richard.local>
2014-09-08 17:58:58 +0200

    Changed range checker to suit banner font size

--- inc/options-framework/includes/class-options-sanitization.php
+++ inc/options-framework/includes/class-options-sanitization.php
@@ -308,7 +308,7 @@ function of_sanitize_hex( $hex, $default = '' ) {
  */
 
 function of_recognized_font_sizes() {
-	$sizes = range( 9, 71 );
+	$sizes = range( 9, 100 );
 	$sizes = apply_filters( 'of_recognized_font_sizes', $sizes );
 	$sizes = array_map( 'absint', $sizes );
 	return $sizes;
