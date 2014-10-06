adb335f684039374ab17cea3139883ef84bc6a67
richlloydmiles <Lightspeed@richard.local>
2014-10-06 09:32:59 +0200

    another attempt, possibly final fix to bug herd issue

--- assets/js/custom/general.js
+++ assets/js/custom/general.js
@@ -1,9 +1,3 @@
 jQuery(document).ready(function($) {
 	$('table#wp-calendar').addClass('table');
 });
-
-jQuery(document).ready(function($) {
-	jQuery('._BH_flag').css('z-index' , 2147483647);
-	console.log(jQuery('._BH_flag'));
-});
-
