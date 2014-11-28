ae4286a9bf429f3d3f2e870492653b8e9f030361
Warwick Booth <krugazul@gmail.com>
2014-11-28 16:15:01 +0200

    no message

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -88,7 +88,7 @@ if ( ! function_exists( 'lsx_post_meta' ) ) {
 function lsx_post_meta() {
 	if ( is_page() && ! is_page_template( 'page-templates/template-blog.php' ) ) { return; }
 
-	$post_info = '<span class="small">' . __( 'By', 'lsx' ) . '</span> ' . the_author() . ' <span class="small">' . _x( 'on', 'post datetime', 'lsx' ) . '</span> ' . the_date() . ' <span class="small">' . __( 'in', 'lsx' ) . '</span> ' . the_category(', ');
+	$post_info = '<span class="small">' . __( 'By', 'lsx' ) . '</span> ' . get_the_author() . ' <span class="small">' . _x( 'on', 'post datetime', 'lsx' ) . '</span> ' . the_date($d = '', $before = '', $after = '', false) . ' <span class="small">' . __( 'in', 'lsx' ) . '</span> ' . get_the_category(', ');
 	
 	printf( '<div class="post-meta">%s</div>' . "\n", $post_info );
 
