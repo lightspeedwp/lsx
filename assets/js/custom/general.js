ae6d43117239dad769dc8d07151b84490a9a1d04
Raphael Segerman <rafi@feedmymedia.com>
2014-12-02 11:09:55 +0200

    app styles, header styles and scripts

--- assets/js/custom/general.js
+++ assets/js/custom/general.js
@@ -1,3 +1,11 @@
 jQuery(document).ready(function($) {
 	$('table#wp-calendar').addClass('table');
+
+	jQuery(window).scroll(function(){
+	    if(jQuery(window).scrollTop() > 0) {
+	        jQuery('header.banner').addClass('scrolled');
+	    } else {
+	        jQuery('header.banner').removeClass('scrolled');
+	    }
+	});
 });
