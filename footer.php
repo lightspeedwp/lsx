87db05e92ecf8f850f58554d661e9ee5c36b98c8
Raphael Segerman <rafi@feedmymedia.com>
2014-12-02 15:10:19 +0200

    footer markup and sass

--- footer.php
+++ footer.php
@@ -13,37 +13,39 @@
 
 	<?php lsx_footer_before(); ?>
 
-	<footer class="content-info container" role="contentinfo">
-		<div class="row">
+	<footer class="content-info" role="contentinfo">
+		<div class="container">
+			<div class="row">
+			    	<div class="col-sm-12">
+	  					    <div class="footer-menu">
+								<?php 
+								
+									if(!is_user_logged_in()){
+										if(has_nav_menu('footer-menu')){
+											wp_nav_menu(array('theme_location' => 'footer-menu'));
+										}
+									}else{
+										if(has_nav_menu('footer_logged_in')){
+											wp_nav_menu(array('theme_location' => 'footer_logged_in'));
+										}
+									}				
+								?>
+								<div class="clearfix"></div>
+							</div>		    	
+			    	</div>
+			 </div>
+
+		  	<div class="row">
 		    	<div class="col-sm-12">
-  					    <div class="footer-menu">
-							<?php 
-							
-								if(!is_user_logged_in()){
-									if(has_nav_menu('footer-menu')){
-										wp_nav_menu(array('theme_location' => 'footer-menu'));
-									}
-								}else{
-									if(has_nav_menu('footer_logged_in')){
-										wp_nav_menu(array('theme_location' => 'footer_logged_in'));
-									}
-								}				
-							?>
-							<div class="clearfix"></div>
-						</div>		    	
+
+		    		<?php lsx_footer_top(); ?>
+
+		      		<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> All Rights Reserved.</p>
+
+		      		<?php lsx_footer_bottom(); ?>
 		    	</div>
-		 </div>
-
-	  	<div class="row">
-	    	<div class="col-sm-12">
-
-	    		<?php lsx_footer_top(); ?>
-
-	      		<p>&copy; <?php echo date('Y'); ?> <?php bloginfo('name'); ?> All Rights Reserved.</p>
-
-	      		<?php lsx_footer_bottom(); ?>
-	    	</div>
-	  	</div>
+		  	</div>
+		</div>
 	</footer>
 
 	<?php lsx_footer_after(); ?>
