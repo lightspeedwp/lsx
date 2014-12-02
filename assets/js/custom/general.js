eb526f9ddba589df59f8ce6b564d785224998d83
Raphael Segerman <rafi@feedmymedia.com>
2014-12-02 12:07:47 +0200

    more header/content margins

--- assets/js/custom/general.js
+++ assets/js/custom/general.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 	$('table#wp-calendar').addClass('table');
 
 	jQuery(window).scroll(function(){
-	    if(jQuery(window).scrollTop() > 0) {
+	    if(jQuery(window).scrollTop() > 200) {
 	        jQuery('header.banner').addClass('scrolled');
 	    } else {
 	        jQuery('header.banner').removeClass('scrolled');
