f41156d6cc43f7373116d4491d44ddab63ffbf3d
Raphael Segerman <rafi@feedmymedia.com>
2014-12-04 09:56:18 +0200

    scroll js

--- assets/js/custom/general.js
+++ assets/js/custom/general.js
@@ -2,7 +2,7 @@ jQuery(document).ready(function($) {
 	$('table#wp-calendar').addClass('table');
 
 	jQuery(window).scroll(function(){
-	    if(jQuery(window).scrollTop() > 140) {
+	    if(jQuery(window).scrollTop() > 100) {
 	        jQuery('header.banner').addClass('scrolled');
 	    } else {
 	        jQuery('header.banner').removeClass('scrolled');
