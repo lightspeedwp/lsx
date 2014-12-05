4f0818b517a7889db841a48cfd578898423b7afd
Raphael Segerman <rafi@feedmymedia.com>
2014-12-05 14:52:28 +0200

    lank

--- content-portfolio-single.php
+++ content-portfolio-single.php
@@ -4,21 +4,47 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+<?php lsx_entry_before(); ?>
 
-		<?php echo get_the_term_list( $post->ID, 'jetpack-portfolio-type', '<span class="portfolio-entry-meta"><i class="fa fa-tags"></i>', _x(', ', 'Used between list items, there is a space after the comma.', 'lsx' ), '</span>' ); ?>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+	<?php lsx_entry_top(); ?>
+
+	<?php if ( is_single() ) { ?>
+
+	<header class="page-header">
+		<?php if ( lsx_get_option( 'single_thumbnails') && has_post_thumbnail() ) {
+			the_post_thumbnail( 'thumbnail-single' );
+		} ?>
+		<h1 class="page-title"><?php the_title(); ?></h1>		
 	</header><!-- .entry-header -->
+	<?php } else { ?>
+	<header class="entry-header">
+		<?php if ( lsx_get_option( 'single_thumbnails') && has_post_thumbnail() ) {
+			the_post_thumbnail( 'thumbnail-single' );
+		} ?>
+		<h1 class="entry-title"><?php the_title(); ?></h1>		
+	</header><!-- .entry-header -->
+	<?php } ?>
+	<?php lsx_post_meta(); ?>
+	<?php echo get_the_tag_list('<p><i class="fa fa-tags"></i> ',', ','</p>'); ?>
 
 	<div class="entry-content">
-		<?php the_content(); ?>
-		<?php
-			wp_link_pages( array(
-				'before'   => '<div class="page-links clear">',
-				'after'    => '</div>',
-				'pagelink' => '<span class="page-link">%</span>',
-			) );
-		?>
+		
+		<?php if ( ! is_singular() ) {
+			the_excerpt();
+		} else {
+			the_content(); 
+		} ?>
+		<?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'lsx'), 'after' => '</p></nav>')); ?>
 	</div><!-- .entry-content -->
+
+	<?php edit_post_link( __( 'Edit', 'lsx' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+
+	<?php lsx_post_nav(); ?>
+	
+	<?php lsx_entry_bottom(); ?>
+
 </article><!-- #post-## -->
+
+<?php lsx_entry_after(); ?>
