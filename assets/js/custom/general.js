4114bc81c4ae83055b77cc4ae3278b46dfc644d0
Raphael Segerman <rafi@feedmymedia.com>
2014-12-05 12:37:24 +0200

    menu

--- assets/js/custom/general.js
+++ assets/js/custom/general.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function($) {
 	$('table#wp-calendar').addClass('table');
-	jQuery(window).resize();
-
+	
 	jQuery(window).scroll(function(){
 	    if(jQuery(window).scrollTop() > 100) {
 	        jQuery('header.banner').addClass('scrolled');
@@ -9,4 +8,6 @@ jQuery(document).ready(function($) {
 	        jQuery('header.banner').removeClass('scrolled');
 	    }
 	});
+
+	jQuery(window).resize();
 });
