ae8853e38627ab99b1c1466ee22919a8aa8f96ed
Raphael Segerman <rafi@feedmymedia.com>
2014-12-10 12:14:45 +0200

    bugherd

--- content-single.php
+++ content-single.php
@@ -27,10 +27,8 @@
 	</header><!-- .entry-header -->
 	<?php } ?>
 	<?php lsx_post_meta(); ?>
-	<?php echo get_the_tag_list('<p><i class="fa fa-tags"></i> ',', ','</p>'); ?>
 
 	<div class="entry-content">
-		
 		<?php if ( ! is_singular() ) {
 			the_excerpt();
 		} else {
@@ -39,6 +37,10 @@
 		<?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'lsx'), 'after' => '</p></nav>')); ?>
 	</div><!-- .entry-content -->
 
+	<div class="post-tags">
+		<?php echo get_the_tag_list('<p><i class="fa fa-tags"></i> ',', ','</p>'); ?>
+	</div>
+	
 	<?php edit_post_link( __( 'Edit', 'lsx' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
 
 	<?php lsx_post_nav(); ?>
