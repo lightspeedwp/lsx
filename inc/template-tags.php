d4db71c4a5bad2143ed7dbe7a6abaaa20fb390f6
Warwick Booth <krugazul@gmail.com>
2015-01-08 13:30:09 +0200

    Removed the paging nav template tags

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -175,32 +175,59 @@ if ( ! function_exists( 'lsx_paging_nav' ) ) :
 			wp_pagenavi();
 		}else{
 		
-			$args = array(
-					'prev_text'          => __( '<span class="meta-nav">&larr;</span> Older posts', 'lsx' ),
-					'next_text'          => __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'lsx' ),
-					'screen_reader_text' => ' '
-			);
-			the_post_navigation($args);
+			?>
+			<nav class="navigation paging-navigation" role="navigation">
+				<h1 class="screen-reader-text"><?php _e( 'Posts navigation', 'lsx' ); ?></h1>
+				<div class="nav-links">
+		
+					<?php if ( get_next_posts_link() ) : ?>
+					<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'lsx' ) ); ?></div>
+					<?php endif; ?>
+		
+					<?php if ( get_previous_posts_link() ) : ?>
+					<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'lsx' ) ); ?></div>
+					<?php endif; ?>
+		
+				</div><!-- .nav-links -->
+			</nav><!-- .navigation -->
+			<?php
 		}
 	}
 endif;
 
 if ( ! function_exists( 'lsx_post_nav' ) ) :
-	/**
-	 * Display navigation to next/previous post when applicable.
-	 *
-	 * @return void
-	 */
-	function lsx_post_nav() {
-		// Don't print empty markup if there's nowhere to navigate.
-		
-		$args = array(
-				'prev_text'          => '<span class="meta-nav">&larr;</span> %title',
-				'next_text'          => '%title <span class="meta-nav">&rarr;</span>',
-				'screen_reader_text' => ' ',
-		);
-		the_post_navigation($args);
+/**
+ * Display navigation to next/previous post when applicable.
+ *
+ * @return void
+ */
+function lsx_post_nav() {
+	// Don't print empty markup if there's nowhere to navigate.
+	$previous = ( is_attachment() ) ? get_post( get_post()->post_parent ) : get_adjacent_post( false, '', true );
+	$next     = get_adjacent_post( false, '', false );
+
+	if ( ! $next && ! $previous ) {
+		return;
 	}
+	?>
+	<nav class="navigation post-navigation" role="navigation">
+		<div class="nav-links pager row">
+
+			<?php
+				$previous_post = get_previous_post_link( '%link', _x( '<div class="previous"><span class="meta-nav">&larr;</span> %title</div>', 'Previous post link', 'lsx' ) );
+				$previous_post = str_replace('<a','<a class="col-sm-6"',$previous_post);
+				echo $previous_post;
+			?>
+			<?php
+				$next_post = get_next_post_link(     '%link', _x( '<div class="next">%title <span class="meta-nav">&rarr;</span></div>', 'Next post link',     'lsx' ) );
+				$next_post = str_replace('<a','<a class="col-sm-6"',$next_post);
+				echo $next_post;
+			?>
+
+		</div><!-- .nav-links -->
+	</nav><!-- .navigation -->
+	<?php
+}
 endif;
 
 if ( ! function_exists( 'lsx_posted_on' ) ) :
