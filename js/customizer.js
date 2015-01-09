2e5fba8f542519c701e63e39daafcd511dd40119
David Cramer <david@digilab.co.za>
2015-01-09 11:21:02 +0200

    merge

--- js/customizer.js
+++ js/customizer.js
@@ -11,10 +11,6 @@
 		// push message
 		message.bind( function( value ) {
 			// Do stuff with pushed message value
-<<<<<<< HEAD
-
-=======
->>>>>>> d66d3db14d1c9d66dbba1f307c9d05c94b09d93e
 			$( 'a' ).css( 'color' , value );
 		} );
 	} );	
