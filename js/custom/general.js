b6e73dbdaa0f6f6da4a81b511831eb76ff5f2623
Raphael Segerman <rafi@feedmymedia.com>
2015-01-14 13:33:25 +0200

    sigh, reverting again

--- js/custom/general.js
+++ js/custom/general.js
@@ -14,26 +14,7 @@ jQuery(document).ready(function($) {
     }, function() {
         $(this).removeClass('zoom');
     });
-/*
-    //Masonry blocks
-	$blocks = $(".lsx-portfolio");
-
-	$blocks.imagesLoaded(function(){
-		$blocks.masonry({
-			itemSelector: '.jetpack-portfolio'
-		});
-
-		// Fade blocks in after images are ready (prevents jumping and re-rendering)
-		$(".jetpack-portfolio").fadeIn();
-	});
-	
-	$(document).ready( function() { setTimeout( function() { $blocks.masonry(); }, 500); });
-
-	$(window).resize(function () {
-		$blocks.masonry();
-	});
-
-	/*
+    
 	$('.lsx-portfolio').masonry({
 	    itemSelector: '.jetpack-portfolio',
 	    singlemode: true,
@@ -57,32 +38,6 @@ jQuery(document).ready(function($) {
 		    }
 	    });
     });
-*/	
-	$(window).load(function() {
-	    $('.lsx-portfolio').masonry({
-		    itemSelector: '.jetpack-portfolio',
-		    singlemode: true,
-		    isAnimated: true,
-		    animationOptions: {
-		        duration: 700,
-		        easing: 'linear',
-		        queue: false
-		    }
-		});
-
-		$('.lsx-portfolio').imagesLoaded( function(){
-		    $('.lsx-portfolio').masonry({
-				itemSelector: '.jetpack-portfolio',
-				isAnimated: true,
-				isFitWidth: true,
-				animationOptions: {
-			        duration: 700,
-			        easing: 'linear',
-			        queue: false
-			    }
-		    });
-	    });
-	});
 });
 
 	
