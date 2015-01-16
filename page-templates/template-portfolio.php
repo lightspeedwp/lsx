85a1b1b0f230cc4eb305d15a07a698e34b4645f0
Warwick <warwick@feedmymedia.com>
2015-01-16 16:48:52 +0200

    no message

--- page-templates/template-portfolio.php
+++ page-templates/template-portfolio.php
@@ -34,7 +34,6 @@ get_header(); ?>
 				<?php $j=1;
 				$types = get_terms('jetpack-portfolio-type');
 				
-				print_r($types);
 				if(is_array($types)){
 					foreach ($types as $type) {
 						$a = '<li><a href="#" data-filter=".'.$type->slug.'">';
