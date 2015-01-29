6f47951c55c7c29d58d6ee2c0899ff02240490ad
David Cramer <david@digilab.co.za>
2015-01-29 17:17:42 +0200

    don't know how  this works but lets see

--- inc/extras.php
+++ inc/extras.php
@@ -255,14 +255,14 @@ function lsx_get_thumbnail($size){
 		$tablet = wp_get_attachment_image_src( $post_thumbnail_id, 'medium' );
 		$mobile = wp_get_attachment_image_src( $post_thumbnail_id, 'thumbnail-wide' );
 		
-		$img = '<img class="attachment-responsive wp-post-image lsx-responsive" src="" data-desktop="'.$thumbnail[0].'" data-tablet="'.$tablet[0].'" data-mobile="'.$mobile[0].'" />';
+		$img = '<img class="attachment-responsive wp-post-image lsx-responsive" src="'.$thumbnail[0].'" data-desktop="'.$thumbnail[0].'" data-tablet="'.$tablet[0].'" data-mobile="'.$mobile[0].'" />';
 	
 	}elseif('thumbnail-wide' == $size){
 		$thumbnail = wp_get_attachment_image_src( $post_thumbnail_id, $size );
 		$tablet = wp_get_attachment_image_src( $post_thumbnail_id, 'medium' );
 		$mobile = wp_get_attachment_image_src( $post_thumbnail_id, $size );	
 
-		$img = '<img class="attachment-responsive wp-post-image lsx-responsive" src="" data-desktop="'.$thumbnail[0].'" data-tablet="'.$tablet[0].'" data-mobile="'.$mobile[0].'" />';
+		$img = '<img class="attachment-responsive wp-post-image lsx-responsive" src="'.$thumbnail[0].'" data-desktop="'.$thumbnail[0].'" data-tablet="'.$tablet[0].'" data-mobile="'.$mobile[0].'" />';
 	}
 	return $img;
 }
