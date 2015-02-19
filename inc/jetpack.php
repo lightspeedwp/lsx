c77eef4ab4651875eb3616fed52eedb42c8490de
Warwick <warwick@feedmymedia.com>
2015-02-19 12:38:53 +0200

    no message

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -133,7 +133,7 @@ add_filter( 'wp', 'lsx_remove_single_portfolio_related_posts', 20 );
 function lsx_portfolio_related_posts(){
 	if(class_exists('Jetpack_RelatedPosts')){ ?>
 		<div class="row">
-			<div class="col-md-12">	
+			<div class="col-md-12">
 				<?php echo do_shortcode('[jetpack-related-posts]'); ?>
 			</div>
 		</div>			
