3c357732d30c85a4cb8353ce2071e37ea48202e1
Warwick <warwick@feedmymedia.com>
2015-03-10 15:11:40 +0200

    Removed the wp_enqueue_script('query'), its not needed when including query as a dependency

--- inc/scripts.php
+++ inc/scripts.php
@@ -19,7 +19,6 @@ function lsx_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 
-	wp_enqueue_script('jquery');
 	wp_enqueue_script('lsx_scripts', get_template_directory_uri() . '/js/scripts.min.js', false, 'c9f983e2965b9c7888dac272e56c4f4b', true);
 	wp_enqueue_script('modernizr', get_template_directory_uri() . '/js/vendor/modernizr-2.7.0.min.js', false, null, false);
 	wp_enqueue_script('isotope', get_template_directory_uri().'/js/jquery.isotope.min.js', array('jquery'), '1.3.110525', true);
