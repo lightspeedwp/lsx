61d500c71d512b7d66302988e08f4517c9221171
Warwick <warwick@feedmymedia.com>
2015-03-23 10:28:53 +0200

    Added in a filter hook so plugin can change the number of columns on their post type archive and page templates.

--- inc/scripts.php
+++ inc/scripts.php
@@ -35,13 +35,9 @@ function lsx_scripts() {
 	$param_array = array(
 			'is_portfolio' => $is_portfolio
 	);
-	
 	//Set the columns for the archives
-	$colums = 3;
-	$dynamic_column_post_types = apply_filters('lsx_archive_column_number',array());
-	if(is_post_type_archive($dynamic_column_post_types)){
-		
-	}
+	$param_array['columns'] = apply_filters('lsx_archive_column_number',3);
+	
 	
 	wp_localize_script( 'lsx_custom', 'lsx_params', $param_array );
 	
