a0f646f4081907cc54ec4be7aadf2e1164173d37
Warwick <warwick@feedmymedia.com>
2015-04-01 11:40:01 +0200

    Set the portfolio archive to only call portfolio items with images

--- inc/jetpack.php
+++ inc/jetpack.php
@@ -78,6 +78,14 @@ add_action('wp_head','lsx_portfolio_infinite_scroll',1000);
 function lsx_portfolio_archive_pagination( $query ) {
 	if ( $query->is_post_type_archive(array('jetpack-portfolio')) && $query->is_main_query() ) {
 		$query->set( 'posts_per_page', '-1' );
+		
+		$meta_query = array(
+			array(
+				'key'	=> '_thumbnail_id',
+				'compare'	=> 'EXISTS'
+			)
+		);
+		$query->set( 'meta_query', $meta_query);
 	}
 }
 add_action( 'pre_get_posts', 'lsx_portfolio_archive_pagination' );
