3a60a51850f15ae4abb315f1b87c37ef9761db48
Raphael Segerman <rafi@feedmymedia.com>
2015-04-10 12:27:01 +0200

    hero unit and section heading mixins/classes, page headings

--- page.php
+++ page.php
@@ -20,6 +20,10 @@ get_header(); ?>
 
 			<?php lsx_content_top(); ?>
 
+			<header class="page-header">
+				<h1 class="page-title"><?php the_title(); ?></h1>		
+			</header><!-- .entry-header -->
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'content', 'page' ); ?>
