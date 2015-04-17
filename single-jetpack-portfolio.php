2e544f4ce4e57e6cf2a844e59b588d8ab6108644
Raphael Segerman <rafi@feedmymedia.com>
2015-04-17 15:20:05 +0200

    1 moar

--- single-jetpack-portfolio.php
+++ single-jetpack-portfolio.php
@@ -7,6 +7,10 @@
 
 get_header(); ?>
 
+	<div id="secondary" class="widget-area col-md-4" role="complementary">
+		<?php lsx_portfolio_meta(); ?>
+	</div><!-- #secondary -->
+
 	<div id="primary" class="content-area single-portfolio col-sm-8">
 
 		<?php lsx_content_before(); ?>
@@ -26,9 +30,5 @@ get_header(); ?>
 		<?php lsx_content_after(); ?>
 		
 	</div><!-- #primary -->
-
-	<div id="secondary" class="widget-area col-md-4" role="complementary">
-		<?php lsx_portfolio_meta(); ?>
-	</div><!-- #secondary -->
 
 <?php get_footer(); ?>
