bec3327ea8d4ab3ebedceea315f9ebe639373f92
Raphael Segerman <rafi@feedmymedia.com>
2015-06-17 15:17:23 +0200

    hopefully the correct loop

--- page-templates/template-metaplate.php
+++ page-templates/template-metaplate.php
@@ -5,8 +5,9 @@
  * @package lsx
  */
 get_header() ?>
-		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile;
-		endif; ?>	
+	<?php while ( have_posts() ) : the_post(); ?>
+
+		<?php get_template_part( 'content', 'page' ); ?>
+
+	<?php endwhile; // end of the loop. ?>	
 <?php get_footer(); ?>
