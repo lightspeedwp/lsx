47e94246f130aa524dfab953c625a5fe17137df4
Raphael Segerman <rafi@feedmymedia.com>
2015-06-17 15:15:41 +0200

    stupid

--- page-templates/template-metaplate.php
+++ page-templates/template-metaplate.php
@@ -6,7 +6,7 @@
  */
 get_header() ?>
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-			the_content();
+			<?php the_content(); ?>
 		<?php endwhile;
 		endif; ?>	
 <?php get_footer(); ?>
