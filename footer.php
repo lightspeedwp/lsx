e99ee982967e6cf11b76ac0601e66791a6b3ae68
Warwick <warwick@feedmymedia.com>
2015-06-17 13:25:44 +0200

    Changing the page templates conditional

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
  * @package lsx
  */
 ?>
-<?php if ( !is_singular('landing-page') && !is_page_template('template-metaplate.php') ) { ?>
+<?php if ( !is_singular('landing-page') && !is_page_template('page-templates/template-metaplate.php') ) { ?>
 
 			</div><!-- .content -->
 		</div><!-- wrap -->
