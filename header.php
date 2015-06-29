f3c9bd8ddde886834c70622a0730a50904233123
Raphael Segerman <rafi@feedmymedia.com>
2015-06-29 13:49:45 +0200

    adding alternate custom metaplate template to theme to use for custom signup pages before we have a dynamic solution in place

--- header.php
+++ header.php
@@ -56,7 +56,7 @@ global $lsx_options;
 			<div class="wrap container" role="document">
 				<div class="content role row">
 
-	<?php } elseif ( is_page_template('page-templates/template-metaplate.php') && is_page('signup') ) { ?>
+	<?php } elseif ( is_page_template('page-templates/template-metaplate-alt.php') ) { ?>
 
 		<?php lsx_header_before(); ?>
 
