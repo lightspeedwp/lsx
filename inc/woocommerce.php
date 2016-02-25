1e186dade39f6ae296ee0303624bd986308cb3f9
Warwick <warwick@feedmymedia.com>
2016-02-25 12:34:28 +0200

    Added in two functions for adding in the #primary and #main divs to the woo commerce pages.

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -9,11 +9,44 @@
 /*
  * Hooks
  */
-/**
- * Adding WooCommerce Hooks and Theme Support
- */
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
 
-add_action('woocommerce_before_main_content', 'lsx_content_before', 10);
-add_action('woocommerce_after_main_content', 'lsx_content_after', 10);
+add_action('woocommerce_before_main_content', 'lsx_woocommerce_before_content', 10);
+add_action('woocommerce_after_main_content', 'lsx_woocommerce_after_content', 10);
+
+/*
+ * Layout
+ */
+
+/**
+ * Adds the top and primary divs for the layout.
+ * @package lsx
+ * @subpackage woocommerce
+ * @category 	layout
+ */
+function lsx_woocommerce_before_content(){ ?>
+	<div id="primary" class="content-area <?php echo lsx_main_class(); ?>">
+
+		<?php lsx_content_before(); ?>
+
+		<main id="main" class="site-main" role="main">
+		
+		<?php lsx_content_top(); ?>
+<?php }
+
+/**
+ * Adds the closing divs for primary and main to woocommerce.
+ * @package lsx
+ * @subpackage woocommerce
+ * @category 	layout
+ */
+function lsx_woocommerce_after_content(){ ?>
+		<?php lsx_content_bottom(); ?>
+
+		</main><!-- #main -->
+
+		<?php lsx_content_after(); ?>
+		
+	</div><!-- #primary -->
+<?php }
