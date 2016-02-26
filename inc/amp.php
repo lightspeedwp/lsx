80f5eb70eb2bdc91726737cf4e1ac331dd8f0bc9
Warwick <warwick@feedmymedia.com>
2016-02-26 09:28:57 +0200

    Fixing the AMP if conditional statements

--- inc/amp.php
+++ inc/amp.php
@@ -29,14 +29,13 @@ add_action( 'amp_init', 'lsx_amp_init' );
  * @subpackage amp
  * @category template
  */
-function lxs_amp_custom_template === $type) {
+function lxs_amp_custom_template( $file, $type, $post ) {
+    if ( 'single' === $type) {
         $file = get_template_directory() . '/mobile-templates/single-'.$post->post_type.'.php';
     }
     return $file;
 }
 add_filter( 'amp_post_template_file', 'lxs_amp_custom_template', 10, 3 );
-
-
 
 /**
  * @package     YoastSEO_AMP_Glue
@@ -55,106 +54,103 @@ add_filter( 'amp_post_template_file', 'lxs_amp_custom_template', 10, 3 );
 /**
  * Defines the YoastSEO AMP class. In a wrapper function so we can overwrite this from within Yoast SEO later on.
  */
-function lsx_yoast_seo_amp_glue_define() {
-	if ( ! class_exists( 'YoastSEO_AMP' ) ) {
+if ( ! class_exists( 'YoastSEO_AMP' ) ) {
+	/**
+	 * This class improves upon the AMP output by the default WordPress AMP plugin using Yoast SEO metadata.
+	 */
+	class YoastSEO_AMP {
 		/**
-		 * This class improves upon the AMP output by the default WordPress AMP plugin using Yoast SEO metadata.
+		 * @var WPSEO_Frontend
 		 */
-		class YoastSEO_AMP {
-			/**
-			 * @var WPSEO_Frontend
-			 */
-			private $front;
-			/**
-			 * @var array
-			 */
-			private $options;
-			/**
-			 * YoastSEO_AMP constructor.
-			 */
-			public function __construct() {
-				$this->front   = WPSEO_Frontend::get_instance();
-				$this->options = WPSEO_Options::get_all();
-				add_filter( 'amp_post_template_data', array( $this, 'fix_amp_post_data' ), 10, 2 );
-				add_filter( 'amp_post_template_metadata', array( $this, 'fix_amp_post_metadata' ), 10, 2 );
+		private $front;
+		/**
+		 * @var array
+		 */
+		private $options;
+		/**
+		 * YoastSEO_AMP constructor.
+		 */
+		public function __construct() {
+			$this->front   = WPSEO_Frontend::get_instance();
+			$this->options = WPSEO_Options::get_all();
+			add_filter( 'amp_post_template_data', array( $this, 'fix_amp_post_data' ), 10, 2 );
+			add_filter( 'amp_post_template_metadata', array( $this, 'fix_amp_post_metadata' ), 10, 2 );
+		}
+		/**
+		 * Fix the basic AMP post data
+		 *
+		 * @param array $data
+		 * @param object $post
+		 *
+		 * @return array
+		 */
+		public function fix_amp_post_data( $data, $post ) {
+			$data['canonical_url'] = $this->front->canonical( false );
+			return $data;
+		}
+		/**
+		 * Fix the AMP metadata for a post
+		 *
+		 * @param array $metadata
+		 * @param object $post
+		 *
+		 * @return array
+		 */
+		public function fix_amp_post_metadata( $metadata, $post ) {
+			$this->build_organization_object( $metadata );
+			$desc = $this->front->metadesc( false );
+			if ( $desc ) {
+				$metadata['description'] = $desc;
 			}
-			/**
-			 * Fix the basic AMP post data
-			 *
-			 * @param array $data
-			 * @param object $post
-			 *
-			 * @return array
-			 */
-			public function fix_amp_post_data( $data, $post ) {
-				$data['canonical_url'] = $this->front->canonical( false );
-				return $data;
+			$og_img = $this->get_image_object( WPSEO_Meta::get_value( 'opengraph-image', $post->ID ) );
+			if ( is_array( $og_img ) ) {
+				$metadata['image'] = $og_img;
 			}
-			/**
-			 * Fix the AMP metadata for a post
-			 *
-			 * @param array $metadata
-			 * @param object $post
-			 *
-			 * @return array
-			 */
-			public function fix_amp_post_metadata( $metadata, $post ) {
-				$this->build_organization_object( $metadata );
-				$desc = $this->front->metadesc( false );
-				if ( $desc ) {
-					$metadata['description'] = $desc;
-				}
-				$og_img = $this->get_image_object( WPSEO_Meta::get_value( 'opengraph-image', $post->ID ) );
-				if ( is_array( $og_img ) ) {
-					$metadata['image'] = $og_img;
-				}
-				return $metadata;
+			return $metadata;
+		}
+		/**
+		 * Builds the organization object if needed.
+		 *
+		 * @param array $metadata
+		 */
+		private function build_organization_object( &$metadata ) {
+			// While it's using the blog name, it's actually outputting the company name
+			if ( ! empty( $this->options['company_name'] ) ) {
+				$metadata['publisher']['name'] = $this->options['company_name'];
 			}
-			/**
-			 * Builds the organization object if needed.
-			 *
-			 * @param array $metadata
-			 */
-			private function build_organization_object( &$metadata ) {
-				// While it's using the blog name, it's actually outputting the company name
-				if ( ! empty( $this->options['company_name'] ) ) {
-					$metadata['publisher']['name'] = $this->options['company_name'];
-				}
-				// The logo needs to be 600px wide max, 60px high max
-				$logo = $this->get_image_object( $this->options['company_logo'], array( 600, 60 ) );
-				if ( is_array( $logo ) ) {
-					$metadata['publisher']['logo'] = $logo;
-				}
+			// The logo needs to be 600px wide max, 60px high max
+			$logo = $this->get_image_object( $this->options['company_logo'], array( 600, 60 ) );
+			if ( is_array( $logo ) ) {
+				$metadata['publisher']['logo'] = $logo;
 			}
-			/**
-			 * Builds an image object array from an image URL
-			 *
-			 * @param string $image_url
-			 * @param string|array $size Optional. Image size. Accepts any valid image size, or an array of width
-			 *                                    and height values in pixels (in that order). Default 'full'.
-			 *
-			 * @return bool|array
-			 */
-			private function get_image_object( $image_url, $size = 'full' ) {
-				if ( empty( $image_url ) ) {
-					return false;
-				}
-				$image_id  = attachment_url_to_postid( $image_url );
-				$image_src = wp_get_attachment_image_src( $image_id, $size );
-				if ( is_array( $image_src ) ) {
-					return array(
-						'@type'  => 'ImageObject',
-						'url'    => $image_src[0],
-						'width'  => $image_src[1],
-						'height' => $image_src[2]
-					);
-				}
+		}
+		/**
+		 * Builds an image object array from an image URL
+		 *
+		 * @param string $image_url
+		 * @param string|array $size Optional. Image size. Accepts any valid image size, or an array of width
+		 *                                    and height values in pixels (in that order). Default 'full'.
+		 *
+		 * @return bool|array
+		 */
+		private function get_image_object( $image_url, $size = 'full' ) {
+			if ( empty( $image_url ) ) {
 				return false;
 			}
+			$image_id  = attachment_url_to_postid( $image_url );
+			$image_src = wp_get_attachment_image_src( $image_id, $size );
+			if ( is_array( $image_src ) ) {
+				return array(
+					'@type'  => 'ImageObject',
+					'url'    => $image_src[0],
+					'width'  => $image_src[1],
+					'height' => $image_src[2]
+				);
+			}
+			return false;
 		}
 	}
 }
-add_action( 'plugins_loaded', 'lsx_yoast_seo_amp_glue_define', 99 );
 /**
  * Initialize the Yoast SEO AMP Glue plugin
  */
