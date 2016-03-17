1cd23036ecb45ca4a466ab134cc67839e418a0dc
Raphael Segerman <rafi@feedmymedia.com>
2016-03-17 10:00:03 +0200

    redirecting lessons archive to courses overview

--- inc/sensei.php
+++ inc/sensei.php
@@ -64,8 +64,8 @@ function lsx_sensei_after_content(){ ?>
 /**
  * Removes Sensei plugin styles and enqueues Sensei styles from the theme instead.
  * @package lsx
- * @subpackage woocommerce
- * @category 	styles
+ * @subpackage sensei
+ * @category redirect
  */
 add_filter( 'sensei_disable_styles', '__return_true' );
 
@@ -73,3 +73,17 @@ function lsx_sensei_styles() {
     wp_enqueue_style( 'sensei', get_template_directory_uri() . '/css/sensei.css' );
 }
 add_action( 'wp_enqueue_scripts', 'lsx_sensei_styles' );
+
+/**
+ * Redirects Lessons Archive to Courses Overview
+ * @package lsx
+ * @subpackage woocommerce
+ * @category 	styles
+ */
+function redirect_to_home( $query ){
+    if(is_post_type_archive( 'lesson' ) ) {
+         wp_redirect( home_url() . '/courses-overview' );
+         exit;
+     }
+}
+add_action( 'parse_query', 'redirect_to_home' );
