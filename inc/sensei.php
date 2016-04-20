dc68017b4ad03eb511d6548f120e53286fc112d9
Warwick <warwick@feedmymedia.com>
2016-04-20 22:26:41 +0200

    Moving the module archive description

--- inc/sensei.php
+++ inc/sensei.php
@@ -55,6 +55,10 @@ function lsx_sensei_wp_head(){
 		if(is_tax('module')){
 			remove_action( 'sensei_content_lesson_inside_before', array( 'Sensei_Lesson', 'the_lesson_meta' ), 20 );
 			add_action( 'sensei_content_lesson_inside_before', array( 'Sensei_Lesson', 'the_lesson_meta' ), 40 );
+			
+			remove_action('sensei_content_lesson_inside_before', array('Sensei_Lesson', 'module_archive_description'), 11);
+			add_action('sensei_content_lesson_inside_before', array('Sensei_Lesson', 'module_archive_description'), 50);
+			
 		}
 	}
 		
