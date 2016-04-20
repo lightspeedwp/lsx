5c35b791e49a7aeae0461f211318b190ab95d84c
Warwick <warwick@feedmymedia.com>
2016-04-20 13:07:46 +0200

    Moving the breadcrumbs below the archive title for 1 column layout.

--- archive.php
+++ archive.php
@@ -1,6 +1,4 @@
 <?php
-if ( ! defined( 'ABSPATH' ) ) return; // Exit if accessed directly
-
 /**
  * The template for displaying Archive pages.
  *
@@ -33,6 +31,14 @@ get_header(); ?>
 				<?php echo term_description(); ?>
 
 			</header><!-- .archive-header -->
+			
+			<?php 
+			$layout = get_theme_mod('lsx_layout','2cr');
+			$layout = apply_filters( 'lsx_layout', $layout );
+			if('1c' === $layout && is_search()){
+				lsx_breadcrumbs();
+			}
+			?>			
 
 			<?php /* Start the Loop */ ?>
 			<?php while ( have_posts() ) : the_post(); ?>
