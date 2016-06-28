4c5ef834754a5204c46099cdbe9aac9735077dc1
Fernando Tessmann <nandotess85@gmail.com>
2016-06-28 08:43:39 -0300

    Closes #62 (Remove admin notice from TGM Activation Plugin)

--- inc/config.php
+++ inc/config.php
@@ -124,10 +124,10 @@ function lsx_register_required_plugins() {
 	 * Only uncomment the strings in the config array if you want to customize the strings.
 	 */
 	$config = array(
-		'id'           => 'lsx',                 // Unique ID for hashing notices for multiple instances of TGMPA.
+		'id'           => 'lsx',                   // Unique ID for hashing notices for multiple instances of TGMPA.
 		'default_path' => '',                      // Default absolute path to bundled plugins.
 		'menu'         => 'tgmpa-install-plugins', // Menu slug.
-		'has_notices'  => true,                    // Show admin notices or not.
+		'has_notices'  => false,                   // Show admin notices or not.
 		'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
 		'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
 		'is_automatic' => false,                   // Automatically activate plugins after installation or not.
