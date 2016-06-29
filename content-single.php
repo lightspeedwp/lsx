a69aff130981795dd5570ac15df0f75941baee69
Fernando Tessmann <nandotess85@gmail.com>
2016-06-29 12:04:16 -0300

    Single styling

--- content-single.php
+++ content-single.php
@@ -10,22 +10,7 @@
 
 	<?php lsx_entry_top(); ?>
 
-	<?php if ( is_single() ) { ?>
-		<header class="page-header">
-			<h1 class="page-title"><?php the_title(); ?></h1>		
-		</header><!-- .entry-header -->
-	<?php } else { ?>
-		<header class="entry-header">
-			<h1 class="entry-title"><?php the_title(); ?></h1>		
-		</header><!-- .entry-header -->
-	<?php } 
-
-	if ( is_singular('post') && !has_post_thumbnail() ) { ?>
-		<header class="single-header">
-			<h1 class="single-title"><?php the_title(); ?></h1>		
-		</header><!-- .entry-header -->
-	<?php }
-
+	<?php
 	$format = get_post_format();
 	if ( false === $format ) {
 		$format = 'standard';
@@ -33,14 +18,49 @@
 	$format_link = get_post_format_link($format);
 	$format = lsx_translate_format_to_fontawesome($format);
 	?>
-	<div class="entry-meta">
-		<?php if ( has_post_thumbnail() ) { ?>
-			<a href="<?php echo esc_url($format_link) ?>" class="format-link has-thumb fa fa-<?php echo $format ?>"></a>
-		<?php } else { ?>
-			<a href="<?php echo esc_url($format_link) ?>" class="format-link fa fa-<?php echo $format ?>"></a>
-		<?php } ?>
 
-		<?php lsx_post_meta(); ?>	
+	<?php if ( is_single() ) { ?>
+		<header class="page-header">
+			<h1 class="page-title">
+				<?php if ( has_post_thumbnail() ) { ?>
+					<a href="<?php echo esc_url($format_link) ?>" class="format-link has-thumb fa fa-<?php echo $format ?>"></a>
+				<?php } else { ?>
+					<a href="<?php echo esc_url($format_link) ?>" class="format-link fa fa-<?php echo $format ?>"></a>
+				<?php } ?>
+
+				<span><?php the_title(); ?></span>
+			</h1>
+		</header><!-- .entry-header -->
+	<?php } else { ?>
+		<header class="entry-header">
+			<h1 class="entry-title">
+				<?php if ( has_post_thumbnail() ) { ?>
+					<a href="<?php echo esc_url($format_link) ?>" class="format-link has-thumb fa fa-<?php echo $format ?>"></a>
+				<?php } else { ?>
+					<a href="<?php echo esc_url($format_link) ?>" class="format-link fa fa-<?php echo $format ?>"></a>
+				<?php } ?>
+
+				<span><?php the_title(); ?></span>
+			</h1>
+		</header><!-- .entry-header -->
+	<?php } 
+
+	if ( is_singular('post') ) { ?>
+		<header class="single-header">
+			<h1 class="single-title">
+				<?php if ( has_post_thumbnail() ) { ?>
+					<a href="<?php echo esc_url($format_link) ?>" class="format-link has-thumb fa fa-<?php echo $format ?>"></a>
+				<?php } else { ?>
+					<a href="<?php echo esc_url($format_link) ?>" class="format-link fa fa-<?php echo $format ?>"></a>
+				<?php } ?>
+
+				<span><?php the_title(); ?></span>
+			</h1>
+		</header><!-- .entry-header -->
+	<?php } ?>
+
+	<div class="entry-meta">
+		<?php lsx_post_meta(); ?>
 	</div><!-- .footer-meta -->
 
 	<div class="entry-content">
@@ -53,6 +73,12 @@
 	</div><!-- .entry-content -->
 
 	<footer class="footer-meta">
+		<div class="post-tags-wrapper">
+			<div class="post-tags">
+				<?php echo get_the_tag_list(''); ?>
+			</div>
+		</div>
+
 		<?php
 			// If comments are open or we have at least one comment, load up the comment template
 			if ( comments_open() || '0' != get_comments_number() ) : ?>
@@ -64,20 +90,11 @@
 					?>
 				</div>
 		<?php endif; ?>
-
-		<div class="post-tags-wrapper">
-			<div class="post-tags">
-				<?php echo get_the_tag_list(''); ?>
-			</div>
-		</div>
 	</footer><!-- .footer-meta -->
 	
 	<?php edit_post_link( __( 'Edit', 'lsx' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
 
 	<?php lsx_entry_bottom(); ?>
-
-	<div class="lsx-breaker">
-	</div>
 
 </article><!-- #post-## -->
 
