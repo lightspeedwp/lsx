3777a5bbb30bddf6bd9817aa2fa42265d35afb42
Fernando Tessmann <nandotess85@gmail.com>
2016-08-05 16:07:30 -0300

    Closes #113 (Start to use gulp-wp-pot to generate automatically the .pot file)

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var cleanCSS = require('gulp-clean-css');
 var jshint = require('gulp-jshint');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
+var sort = require('gulp-sort');
+var wppot = require('gulp-wp-pot');
+var gettext = require('gulp-gettext');
 
 gulp.task('default', function() {	 
 	console.log('Use the following commands');
@@ -13,11 +16,12 @@ gulp.task('default', function() {
 	console.log('gulp compile-js			to compile the custom.js to custom.min.js');
 	console.log('gulp watch					to continue watching the files for changes.');
 	console.log('gulp upgrade-components	recopy over the node_module files.');
+	console.log('gulp wordpress-pot			to compile the lsx-mega-menus.pot');
 });
 
 /*   UPGRADE THE COMPONENTS WE USE	*/
 gulp.task('clean-upgrade', function(cb) {
-    del(['sass/bootstrap/*','js/vendor/bootstrap.min.js','js/vendor/jquery.sticky.js'], cb);
+	del(['sass/bootstrap/*','js/vendor/bootstrap.min.js','js/vendor/jquery.sticky.js'], cb);
 });
 
 gulp.task('bootstrap-upgrade', function() {
@@ -32,7 +36,7 @@ gulp.task('jquery-sticky-upgrade', function() {
 });
 
 gulp.task('upgrade-components', ['clean-upgrade'], function() {
-    gulp.start('bootstrap-upgrade');
+	gulp.start('bootstrap-upgrade');
 	gulp.start('jquery-sticky-upgrade');
 });
 
@@ -41,44 +45,44 @@ gulp.task('compile-css', ['compile-css-theme','compile-css-amp','compile-css-woo
 
 gulp.task('compile-css-theme', function() {	
 	gulp.src(['sass/app.scss', 'sass/alegreya_open_sans.scss', 'sass/noto_sans_noto_sans.scss', 'sass/noto_serif_noto_sans.scss', 'sass/raleway_open_sans.scss', 'sass/medium-nav-break.scss'])
-  		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
-  		.pipe(cleanCSS())
- 	 	.pipe(gulp.dest('css/'));
+		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('css/'));
 });
 
 gulp.task('compile-css-amp', function() {	
 	gulp.src('sass/amp.scss')
-  		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
-  		.pipe(cleanCSS())
- 	 	.pipe(gulp.dest('css/'));
+		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('css/'));
 });
 
 gulp.task('compile-css-woocommerce', function() {	
 	gulp.src('sass/woocommerce/woocommerce.scss')
-  		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
-  		.pipe(cleanCSS())
- 	 	.pipe(gulp.dest('css/'));
+		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('css/'));
 });
 
 gulp.task('compile-css-sensei', function() {	
 	gulp.src('sass/sensei/frontend/sensei.scss')
-  		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
-  		.pipe(cleanCSS())
- 	 	.pipe(gulp.dest('css/'));
+		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('css/'));
 });
 
 gulp.task('compile-css-events-calendar', function() {	
 	gulp.src('sass/the-events-calendar.scss')
-  		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
-  		.pipe(cleanCSS())
- 	 	.pipe(gulp.dest('css/'));
+		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('css/'));
 });
 
 gulp.task('compile-css-job-manager', function() {	
 	gulp.src('sass/wp-job-manager.scss')
-  		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
-  		.pipe(cleanCSS())
- 	 	.pipe(gulp.dest('css/'));
+		.pipe( sass().on('error', function (err) {console.log('Error!', err);}) )
+		.pipe(cleanCSS())
+		.pipe(gulp.dest('css/'));
 });
 
 gulp.task('compile-js-theme', function () {
@@ -97,4 +101,32 @@ gulp.task('watch', function() {
 	gulp.watch('sass/sensei/frontend/sensei.scss', ['compile-css-sensei']);
 	gulp.watch('sass/the-events-calendar.scss', ['compile-css-events-calendar']);
 	gulp.watch('sass/wp-job-manager.scss', ['compile-css-job-manager']);
+});
+
+gulp.task('wordpress-pot', function () {
+	gulp.src('**/*.php')
+		.pipe(sort())
+		.pipe(wppot({
+			domain: 'lsx',
+			destFile: 'lsx.pot',
+			package: 'lsx',
+			bugReport: 'https://github.com/lightspeeddevelopment/lsx/issues',
+			team: 'LightSpeed <webmaster@lsdev.biz>'
+		}))
+		.pipe(gulp.dest('languages'));
+
+	gulp.src('**/*.php')
+		.pipe(sort())
+		.pipe(wppot({
+			domain: 'lsx',
+			destFile: 'en_EN.po',
+			package: 'lsx',
+			bugReport: 'https://github.com/lightspeeddevelopment/lsx/issues',
+			team: 'LightSpeed <webmaster@lsdev.biz>'
+		}))
+		.pipe(gulp.dest('languages'));
+
+	gulp.src('languages/en_EN.po')
+		.pipe(gettext())
+		.pipe(gulp.dest('languages'));
 });
