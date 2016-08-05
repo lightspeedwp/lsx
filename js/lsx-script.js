d147ce0dc4862da45672a3eaedd11fbc753791ed
Fernando Tessmann <nandotess85@gmail.com>
2016-08-05 08:50:08 -0300

    Fixed Envira Gallery Isotope layout (with lazy load enable)

--- js/lsx-script.js
+++ js/lsx-script.js
@@ -339,20 +339,24 @@ jQuery(document).ready(function($) {
 		}
 
 		//Lazy Load //Envira Gallery FIX
-		setTimeout(function() {
-			if (jQuery('.lazyload, .lazyloaded').length > 0) {
-				if (typeof envira_isotopes == 'object') {
-					jQuery('.envira-gallery-wrap').each(function() {
-						var id = jQuery(this).attr('id');
-						id = id.replace('envira-gallery-wrap-', '');
+		var enviraIsotopUpdateLayout = function() {
+			jQuery('.envira-gallery-wrap').each(function() {
+				var id = jQuery(this).attr('id');
+				id = id.replace('envira-gallery-wrap-', '');
 
-						if (typeof envira_isotopes[id] == 'object') {
-							envira_isotopes[id].enviratope('layout');
-						}
-					});
+				if (typeof envira_isotopes[id] == 'object') {
+					envira_isotopes[id].enviratope('layout');
 				}
+			});
+		};
+
+		if (jQuery('.lazyload, .lazyloaded').length > 0) {
+			if (typeof envira_isotopes == 'object') {
+				$window.scroll(function(){
+					enviraIsotopUpdateLayout();
+				});
 			}
-		}, 200);
+		}
 
 		//Final load - LAST CODE TO EXECUTE
 		$('body').addClass('html-loaded');
