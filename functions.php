f68c6c141ca1ff134408eaf55757c53e0ddb041c
Fernando Tessmann <nandotess85@gmail.com>
2017-06-08 20:52:30 -0300

    * Dev - LSX Sharing styles moved from LSX to LSX Sharing plugin * Fix - Travis CI file fixed

--- functions.php
+++ functions.php
@@ -6,7 +6,7 @@
  */
 if ( ! defined( 'ABSPATH' ) ) return; // Exit if accessed directly
 
-define('LSX_VERSION', '1.8.7');
+define('LSX_VERSION', '1.8.8');
 
 require get_template_directory() . '/inc/config.php';
 require get_template_directory() . '/inc/customizer.php';
