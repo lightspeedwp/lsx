e76f2ffc968bc5c52efc2f50c513dda1832b4aed
Fernando Tessmann <nandotess85@gmail.com>
2017-11-27 10:29:54 -0600

    * Tweak - Add The Events Calendar compatibility

--- tribe-events/day/single-featured.php
+++ tribe-events/day/single-featured.php
@@ -67,7 +67,7 @@ echo tribe_event_featured_image( null, 'large' );
 	<?php do_action( 'tribe_events_before_the_content' ) ?>
 	<div class="tribe-events-list-event-description tribe-events-content description entry-summary">
 		<?php echo tribe_events_get_the_excerpt(); ?>
-		<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?> &raquo;</a>
+		<a href="<?php echo esc_url( tribe_get_event_link() ); ?>" class="tribe-events-read-more" rel="bookmark"><?php esc_html_e( 'Find out more', 'the-events-calendar' ) ?></a>
 	</div><!-- .tribe-events-list-event-description -->
 
 	<?php do_action( 'tribe_events_after_the_content' ); ?>
