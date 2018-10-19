31fd27f3e16e53090d45a432a69f8e610fcc3fc4
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2018-10-19 12:33:53 -0300

    Addiing custom class for Gutenberg Compatible template (post and pages)

--- includes/gutenberg.php
+++ includes/gutenberg.php
@@ -13,12 +13,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 require_once( get_template_directory() . '/lib/theme-support.php' );
 
-//add_filter( 'body_class', __NAMESPACE__ . '\add_gutenberg_compatible_body_class' );
+add_filter( 'body_class', __NAMESPACE__ . '\add_gutenberg_compatible_body_class' );
 /**
  * Add custom class for Gutenberg Compatible template
  */
 function add_gutenberg_compatible_body_class( $classes ) {
-	if ( is_page_template( 'single.php' ) )
+	if ( is_page_template() || is_archive() )
 		$classes[] = 'gutenberg-compatible-template';
 	return $classes;
 }
