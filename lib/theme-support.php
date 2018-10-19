e96863dda014830431dda7d2a6a586f711825f9c
Virginia Garcia <ve.garcia.ortiz@gmail.com>
2018-10-19 17:21:46 -0300

    Adding admin styles, block styles, base styles, adding a class for when the Gutenberg editor is being used

--- lib/theme-support.php
+++ lib/theme-support.php
@@ -1,28 +1,38 @@
 <?php
 
-add_action( 'after_setup_theme', __NAMESPACE__ . '\theme_support' );
-
-$color_palette = array(
-	array(
-		'name'  => esc_html__( 'Black', 'db-lsx-child-theme' ),
-		'slug' => 'black',
-		'color' => '#2a2a2a',
-	),
-	array(
-		'name'  => esc_html__( 'Gray', 'db-lsx-child-theme' ),
-		'slug' => 'gray',
-		'color' => '#727477',
-	),
-);
-
 /**
  * Enqueue theme CSS and JavaScript .
  */
-function theme_support() {
-	// Add support for full and wide align images.
-	add_theme_support( 'align-wide' );
-	// Add support for styling blocks.
-	add_theme_support( 'wp-block-styles' );
-	// Add support for custom color palettes
-	add_theme_support( 'editor-color-palette', $color_palette );
-}
+if ( ! function_exists( 'theme_support' ) ) :
+	function theme_support() {
+		// Add support for full and wide align images.
+		add_theme_support( 'align-wide' );
+		// Add support for styling blocks.
+		add_theme_support( 'wp-block-styles' );
+		// Add support for custom color scheme.
+		add_theme_support( 'editor-color-palette', array(
+			array(
+				'name'  => __( 'Strong Blue', 'lsx' ),
+				'slug'  => 'strong-blue',
+				'color' => '#27639e',
+			),
+			array(
+				'name'  => __( 'Lighter Blue', 'lsx' ),
+				'slug'  => 'lighter-blue',
+				'color' => '#428bca',
+			),
+			array(
+				'name'  => __( 'Yellow', 'lsx' ),
+				'slug'  => 'light-yellow',
+				'color' => '#f7ae00',
+			),
+			array(
+				'name'  => __( 'Dark Yellow', 'lsx' ),
+				'slug'  => 'dark-yellow',
+				'color' => '#ab7800',
+			),
+		) );
+	}
+endif;
+add_action( 'after_setup_theme', 'theme_support' );
+
