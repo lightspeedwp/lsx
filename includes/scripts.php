c972a5eb4ee2dc628b47393b4b0cf29dd207b9c6
Chris Vancoillie <chris@lsdev.biz>
2019-07-02 07:26:00 +0200

    Update Lora

--- includes/scripts.php
+++ includes/scripts.php
@@ -88,27 +88,35 @@ if ( ! function_exists( 'lsx_scripts_add_fonts' ) ) :
 		$font_styles = '
 			@font-face {
 				font-family: \'Lora\';
-				font-style: normal;
-				font-weight: 400;
-				src: url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Regular.ttf" ) format("truetype");
-			}
-			@font-face {
-				font-family: \'Lora\';
-				font-style: italic;
-				font-weight: 400i;
-				src: url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Italic.ttf" ) format("truetype");
-			}
-			@font-face {
-				font-family: \'Lora\';
+				font-display: swap;
 				font-style: normal;
 				font-weight: 700;
-				src: url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Bold.ttf" ) format("truetype");
+				src: url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Bold.woff2") format("woff2"),
+					 url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Bold.woff") format("woff");
 			}
 			@font-face {
 				font-family: \'Lora\';
+				font-display: swap;
 				font-style: italic;
-				font-weight: 700i;
-				src: url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-BoldItalic.ttf" ) format("truetype");
+				font-weight: 700;
+				src: url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-BoldItalic.woff2") format("woff2"),
+					 url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-BoldItalic.woff") format("woff");
+			}
+			@font-face {
+				font-family: \'Lora\';
+				font-display: swap;
+				font-style: italic;
+				font-weight: 400;
+				src: url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Italic.woff2") format("woff2"),
+					 url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Italic.woff") format("woff");
+			}
+			@font-face {
+				font-family: \'Lora\';
+				font-display: swap;
+				font-style: normal;
+				font-weight: 400;
+				src: url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Regular.woff2") format("woff2"),
+					 url( "' . get_stylesheet_directory_uri() . '/assets/fonts/lora/Lora-Regular.woff") format("woff");
 			}
 			@font-face {
 				font-family: \'Noto Sans\';
