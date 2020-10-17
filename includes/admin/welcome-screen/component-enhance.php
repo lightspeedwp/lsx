20b8189907faec442e5d28c2b0072506e2987a60
Ashley Shaw <ashley@lsdev.biz>
2020-10-17 23:28:07 +0200

    Update component-enhance.php

--- includes/admin/welcome-screen/component-enhance.php
+++ includes/admin/welcome-screen/component-enhance.php
@@ -50,21 +50,21 @@
 			<div class="row">
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-search/" target="_blank"><?php esc_html_e( 'LSX Search ', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/search/" target="_blank"><?php esc_html_e( 'LSX Search ', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'The LSX Search Extension integrates with the FacetWP plugin to allow for superbly filtered search results for your users. Allow your users to break down their search results using available filters (a.k.a “Facets”). These are created by the FacetWP plugin.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-customizer/" target="_blank"><?php esc_html_e( 'LSX Customizer', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/site-customizer/" target="_blank"><?php esc_html_e( 'LSX Customizer', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'You\'ve installed LSX, so now why not make it all yours? Whether it\'s a small CSS tweak or changing the entire layout of pages, posts or archives, please make use of the Theme Customizer plugin.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-blog-customizer/" target="_blank"><?php esc_html_e( 'LSX Blog Customizer', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/blog-customizer/" target="_blank"><?php esc_html_e( 'LSX Blog Customizer', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'It allows you it to customize various aspects of the the appearance of your blog, posts widgets, archive pages and single posts so they looks exactly how you want.', 'lsx' ); ?></p>
 					</div>
 				</div>
@@ -73,21 +73,21 @@
 			<div class="row">
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-mega-menus/" target="_blank"><?php esc_html_e( 'LSX Mega Menus', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/mega-menus/" target="_blank"><?php esc_html_e( 'LSX Mega Menus', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'Create custom, full-width dropdown menus that contain images, widgets and more that seamlessly tie into your LSX WordPress site.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-sharing/" target="_blank"><?php esc_html_e( 'LSX Sharing', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/sharing/" target="_blank"><?php esc_html_e( 'LSX Sharing', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'LSX Sharing adds buttons to your posts that your readers can use to share your content on Facebook, Twitter and Pinterest.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-blocks/" target="_blank"><?php esc_html_e( 'LSX Blocks', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/blocks/" target="_blank"><?php esc_html_e( 'LSX Blocks', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'The new block editor is here, and with it comes the new block-based page builder. We have built these content blocks so you can simply drag and drop them into your editor, configure to your wishes and you are done!', 'lsx' ); ?></p>
 					</div>
 				</div>
@@ -108,21 +108,21 @@
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-team/" target="_blank"><?php esc_html_e( 'LSX Team', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/team/" target="_blank"><?php esc_html_e( 'LSX Team', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'The LSX Team Extension provides a custom post type that allows you to easily show off the people that make up your business.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-testimonials/" target="_blank"><?php esc_html_e( 'LSX Testimonials', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/testimonials/" target="_blank"><?php esc_html_e( 'LSX Testimonials', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'The LSX Testimonials extension adds the "Testimonials" post type, which you can display front-and-centre on your site.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-videos/" target="_blank"><?php esc_html_e( 'LSX Videos', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/videos/" target="_blank"><?php esc_html_e( 'LSX Videos', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'Adds the "Videos" post type to your LSX website which allows you to display and categorize videos.', 'lsx' ); ?></p>
 					</div>
 				</div>
@@ -132,14 +132,14 @@
 			<div class="row">
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-projects/" target="_blank"><?php esc_html_e( 'LSX Projects', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/projects/" target="_blank"><?php esc_html_e( 'LSX Projects', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'Show off your finest work with the LSX Projects Extension! It adds a "Project" post type, allowing you to categorize and display your Projects on your LSX website.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://lsx.lsdev.biz/" target="_blank"><?php esc_html_e( 'More coming soon…', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/tag/release-posts/" target="_blank"><?php esc_html_e( 'Follow our blog for latest developments', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'We need to find some way of linking to social media if we are going to invite people to follow us.', 'lsx' ); ?></p>
 					</div>
 				</div>
@@ -159,21 +159,21 @@
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-give-payfast-gateway/" target="_blank"><?php esc_html_e( 'LSX Give Payfast Gateway', 'lsx' ); ?></a>
+						<a href="hhttps://www.lsdev.biz/lsx/extensions/lsx-give-payfast-payment-gateway/" target="_blank"><?php esc_html_e( 'LSX Give Payfast Gateway', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'Use the PayFast payment gateway with the GiveWP plugin for collecting donations.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-importer-for-wetu/" target="_blank"><?php esc_html_e( 'LSX Wetu Importer', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/wetu-importer/" target="_blank"><?php esc_html_e( 'LSX Wetu Importer', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'Integrate your LSX Tour Operator website with Wetu, to easily import accommodations, destinations and tours from you Wetu account.', 'lsx' ); ?></p>
 					</div>
 				</div>
 
 				<div class="col-md-4">
 					<div class="box-clean">
-						<a href="https://wordpress.org/plugins/lsx-zoho-crm-addon-for-caldera-forms/" target="_blank"><?php esc_html_e( 'LSX Caldera Forms Zoho Integration', 'lsx' ); ?></a>
+						<a href="https://www.lsdev.biz/lsx/extensions/lsx-caldera-forms-zoho-crm-integration/" target="_blank"><?php esc_html_e( 'LSX Caldera Forms Zoho Integration', 'lsx' ); ?></a>
 						<p><?php esc_html_e( 'Maintaining your customer relationship management system is easy when you integrate with your website contact forms..', 'lsx' ); ?></p>
 					</div>
 				</div>
