ff4b9ab526a5f1fa1f46f785e21fc30d87810519
Jacques vd Horst <demonix77x@gmail.com>
2020-11-16 09:34:43 +0200

    - Removed Viewport Function - Updated Header Meta

--- header.php
+++ header.php
@@ -12,8 +12,8 @@
 	<head>
 		<?php lsx_head_top(); ?>
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		<link rel="profile" href="http://gmpg.org/xfn/11">
-		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1" >
+		<link rel="profile" href="https://gmpg.org/xfn/11">
 		<?php lsx_head_bottom(); ?>
 		<?php wp_head(); ?>
 	</head>
