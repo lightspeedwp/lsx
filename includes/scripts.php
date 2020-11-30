8c0aac99bbc60d7df09058050b90207834f3e119
Jacques v.d Horst <35291167+jacquesvdhorst@users.noreply.github.com>
2020-11-30 09:24:47 +0200

    - Updating Script Function

--- includes/scripts.php
+++ includes/scripts.php
@@ -90,7 +90,7 @@ if ( ! function_exists( 'lsx_scripts_add_scripts' ) ) :
 		wp_enqueue_script( 'slick-lightbox', get_template_directory_uri() . '/assets/js/vendor/slick-lightbox.min.js', array( 'jquery', 'slick' ), LSX_VERSION, true );
 		wp_enqueue_script( 'picturefill', get_template_directory_uri() . '/assets/js/vendor/picturefill.min.js', array(), LSX_VERSION, true );
 
-		if ( defined( 'SCRIPT_DEBUG' ) ) {
+		if ( defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ) {
 			$prefix = 'src/';
 			$suffix = '';
 		} else {
