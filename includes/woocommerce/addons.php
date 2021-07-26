944816e0a3b4423be4b7fa3e8ea50f966d4ddd59
Warwick Booth <warwick@lsdev.biz>
2021-07-26 16:03:26 +0200

    Updating the delivery details section slot.

--- includes/woocommerce/addons.php
+++ includes/woocommerce/addons.php
@@ -10,7 +10,6 @@
  * WooCommerce Order Delivery Date
  */
 if ( function_exists( 'wc_od_get_delivery_date_field_args' ) ) {
-
 	/**
 	 * Change the arguments for the checkout delivery date field.
 	 *
@@ -26,5 +25,20 @@ if ( function_exists( 'wc_od_get_delivery_date_field_args' ) ) {
 		return $args;
 	}
 	add_action( 'wc_od_delivery_date_field_args', 'lsx_wc_delivery_date_args', 10, 2 );
+}
 
+if ( function_exists( 'wc_od_order_delivery_details_args' ) ) {
+	/**
+	 * Change the title of the shipping and delivery title.
+	 *
+	 * @package    lsx
+	 * @subpackage woocommerce
+	 * @param      $args array
+	 * @return     array
+	 */
+	function lsx_wc_delivery_details_args( $args = array() ) {
+		$args['title'] = _x( 'Collection or Delivery Time-slot', 'Delivery date title on checkout', 'lsx' );
+		return $args;
+	}
+	add_action( 'wc_od_order_delivery_details_args', 'lsx_wc_delivery_details_args', 10, 1 );
 }
