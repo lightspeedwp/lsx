aa4af989622ded6b72c84352c99a516313347f58
Justin UpMarketing <abrahamsejustin7@gmail.com>
2023-03-02 12:43:28 +0200

    Update footer-mega-contrast-background.php

--- includes/patterns/footer/footer-mega-contrast-background.php
+++ includes/patterns/footer/footer-mega-contrast-background.php
@@ -1,74 +1,73 @@
 <?php
 /**
- * Title: Footer with text, button, links.
- * Slug: frost/footer-mega-contrast-background
- * Categories: frost-footer
- * Block Types: core/template-part/footer
- * Viewport Width: 1280
+ * Footer with text, links.
  */
-?>
+return array(
+	'title'	  => __( 'Footer Mega Contrast', 'lsx' ),
+	'categories' => array( 'footer' ),
+	'content'	=> '
 <!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|base"}}},"typography":{"fontSize":"18px"},"spacing":{"padding":{"top":"var:preset|spacing|x-large","bottom":"var:preset|spacing|x-large"},"margin":{"top":"0px"}}},"backgroundColor":"contrast","textColor":"base","className":"has-background-color","layout":{"type":"constrained"}} -->
 <div class="wp-block-group alignfull has-background-color has-base-color has-contrast-background-color has-text-color has-background has-link-color" style="margin-top:0px;padding-top:var(--wp--preset--spacing--x-large);padding-bottom:var(--wp--preset--spacing--x-large);font-size:18px"><!-- wp:columns {"align":"wide","style":{"elements":{"link":{"color":[]}}}} -->
 <div class="wp-block-columns alignwide has-link-color"><!-- wp:column {"width":"55%"} -->
 <div class="wp-block-column" style="flex-basis:55%"><!-- wp:heading {"level":4} -->
-<h4 id="our-company"><?php echo esc_html__( 'Our Company', 'frost' ); ?></h4>
+<h4 id="our-company">' .  esc_html__( 'Our Company', 'lsx' ). '</h4>
 <!-- /wp:heading -->
 <!-- wp:paragraph -->
 <p>Quisque aliquam nisl quis metus taylor feugiat. Lorem ipsum dolor sit amet, consectetur adipiscing vestibulum vitae gravida non diam.</p>
 <!-- /wp:paragraph -->
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button {"style":{"border":{"radius":0}},"className":"is-style-fill-base"} -->
-<div class="wp-block-button is-style-fill-base"><a class="wp-block-button__link no-border-radius wp-element-button" href="#"><?php echo esc_html__( 'Learn More', 'frost' ); ?></a></div>
+<div class="wp-block-button is-style-fill-base"><a class="wp-block-button__link no-border-radius wp-element-button" href="#">' .  esc_html__( 'Learn More', 'lsx' ). '</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"15%"} -->
 <div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
-<h4 id="about-us"><?php echo esc_html__( 'About Us', 'frost' ); ?></h4>
+<h4 id="about-us">' .  esc_html__( 'About Us', 'lsx' ). '</h4>
 <!-- /wp:heading -->
 <!-- wp:list {"fontSize":"small"} -->
 <ul class="has-small-font-size"><!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Start Here', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Start Here', 'lsx' ). '</a></li>
 <!-- /wp:list-item -->
 <!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Our Mission', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Our Mission', 'lsx' ). '</a></li>
 <!-- /wp:list-item -->
 <!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Brand Guide', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Brand Guide', 'lsx' ). '</a></li>
 <!-- /wp:list-item -->
 <!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Newsletter', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Newsletter', 'lsx' ). '</a></li>
 <!-- /wp:list-item -->
 <!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Accessibility', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Accessibility', 'lsx' ). '</a></li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"15%"} -->
 <div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
-<h4 id="services"><?php echo esc_html__( 'Services', 'frost' ); ?></h4>
+<h4 id="services">' .  esc_html__( 'Services', 'lsx' ). '</h4>
 <!-- /wp:heading -->
 <!-- wp:list {"fontSize":"small"} -->
 <ul class="has-small-font-size"><!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Web Design', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Web Design', 'lsx' ). '</a></li>
 <!-- /wp:list-item -->
 <!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Development', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Development', 'lsx' ). '</a></li>
 <!-- /wp:list-item -->
 <!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Copywriting', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Copywriting', 'lsx' ). '</a></li>
 <!-- /wp:list-item -->
 <!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Marketing', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Marketing', 'lsx' ). '</a></li>
 <!-- /wp:list-item -->
 <!-- wp:list-item -->
-<li><a href="#"><?php echo esc_html__( 'Social Media', 'frost' ); ?></a></li>
+<li><a href="#">' .  esc_html__( 'Social Media', 'lsx' ). '</a></li>
 <!-- /wp:list-item --></ul>
 <!-- /wp:list --></div>
 <!-- /wp:column -->
 <!-- wp:column {"width":"15%"} -->
 <div class="wp-block-column" style="flex-basis:15%"><!-- wp:heading {"level":4} -->
-<h4 id="connect"><?php echo esc_html__( 'Connect', 'frost' ); ?></h4>
+<h4 id="connect">' .  esc_html__( 'Connect', 'lsx' ). '</h4>
 <!-- /wp:heading -->
 <!-- wp:list {"fontSize":"small"} -->
 <ul class="has-small-font-size"><!-- wp:list-item -->
@@ -89,4 +88,5 @@
 <!-- /wp:list --></div>
 <!-- /wp:column --></div>
 <!-- /wp:columns --></div>
-<!-- /wp:group -->
+<!-- /wp:group -->',
+);
