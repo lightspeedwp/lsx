e3632b65b80d4f9098349523e4c6fa7179bd7201
Warwick <krugazul@gmail.com>
2023-03-03 10:16:30 +0200

    Addin in pre get posts statement for the search

--- includes/config.php
+++ includes/config.php
@@ -23,3 +23,12 @@ function lsx_wpforms_match_button_block( $form_data ) {
 	return $form_data;
 }
 add_filter( 'wpforms_frontend_form_data', 'lsx_wpforms_match_button_block' );
+
+
+function lsx_search_args( $query ) {
+	if ( is_search() ) {
+		$query->set( 'post_type', 'post' );
+	}
+	return $query;
+}
+add_action( 'pre_get_posts', 'lsx_search_args', 10, 1 );
