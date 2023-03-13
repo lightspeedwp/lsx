a5fc4c215a7a8ceef10e0017e556ea999d058144
Warwick <krugazul@gmail.com>
2023-03-13 16:45:25 +0200

    Removing the customizer caps.

--- includes/classes/class-setup.php
+++ includes/classes/class-setup.php
@@ -25,6 +25,9 @@ class Setup {
 	 */
 	public function init() {
 		add_action( 'admin_init', array( $this, 'remove_customizer_menu' ), 10 );
+		add_action( 'admin_bar_menu', array( $this, 'remove_customizer_adminbar_menu', 50 ) );
+	
+		add_filter( 'user_has_cap', array( $this, 'remove_customize_capability' ), 200, 4 );
 	}
 
 	/**
@@ -33,7 +36,7 @@ class Setup {
 	 * @return void
 	 */
 	public function remove_customizer_menu() {
-        remove_action('plugins_loaded', '_wp_customize_include', 10);
+        remove_action( 'plugins_loaded', '_wp_customize_include', 10 );
         remove_action(
             'admin_enqueue_scripts',
             '_wp_customize_loader_settings',
@@ -46,4 +49,30 @@ class Setup {
 		}
 		remove_submenu_page( 'themes.php', $customize_url );
 	}
+
+	/**
+	 * Undocumented function
+	 *
+	 * @return void
+	 */
+	public function remove_customizer_adminbar_menu() {
+		die('hello');
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_customize_menu', 40 );
+	}
+
+	/**
+	 * Removes the customizer capability
+	 *
+	 * @param array $caps
+	 * @param string $cap
+	 * @param array $args
+	 * @param object $wp_user
+	 * @return void
+	 */
+    public function remove_customize_capability( $all_caps = [],$caps = '',$args = [], $wp_user ) {
+		if ( isset( $all_caps['customize'] ) ) {
+			unset( $all_caps['customize'] );
+		}
+        return $all_caps;
+    }
 }
