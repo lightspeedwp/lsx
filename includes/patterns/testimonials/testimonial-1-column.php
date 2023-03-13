f5d8f6813fd072a5054df36e5a567128a6e16876
Justin UpMarketing <abrahamsejustin7@gmail.com>
2023-03-13 12:26:44 +0200

    Update testimonial-1-column.php

--- includes/patterns/testimonials/testimonial-1-column.php
+++ includes/patterns/testimonials/testimonial-1-column.php
@@ -5,7 +5,7 @@
 return array(
 	'title'	  => __( 'Testimonial 1 Col', 'lsx' ),
 	'categories' => array( 'testimonials' ),
-	'content'	=> '<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}},"layout":{"type":"constrained","contentSize":"900px"}} -->
+	'content'	=> '<!-- wp:group {"lock":{"move":true,"remove":true} "align":"full","style":{"spacing":{"padding":{"top":"var:preset|spacing|x-small","right":"var:preset|spacing|x-small","bottom":"var:preset|spacing|x-small","left":"var:preset|spacing|x-small"}}},"layout":{"type":"constrained","contentSize":"900px"}} -->
 <div class="wp-block-group alignfull" style="padding-top:var(--wp--preset--spacing--x-small);padding-right:var(--wp--preset--spacing--x-small);padding-bottom:var(--wp--preset--spacing--x-small);padding-left:var(--wp--preset--spacing--x-small)"><!-- wp:heading {"textAlign":"center","align":"wide","style":{"typography":{"fontStyle":"normal","fontWeight":"700"},"spacing":{"padding":{"bottom":"0","top":"var:preset|spacing|x-small"}}},"className":"wp-block-heading","fontSize":"max-48","anchor":"h-client-testimonial"} -->
 <h2 class="wp-block-heading alignwide has-text-align-center has-max-48-font-size" id="h-client-testimonial" style="padding-top:var(--wp--preset--spacing--x-small);padding-bottom:0;font-style:normal;font-weight:700">Client Testimonial</h2>
 <!-- /wp:heading -->
